c19f50a431c18c7f9ec34bb7ac8e675fd73d7479
Wert-bity77 <kabulovt707@gmail.com>
2024-11-25 16:57:52 +0500

    mainjs index.html
    siroj

---
+++ main.js
@@ -0,0 +1,12 @@
+let fert = prompt('Ismingizni kiriting')
+let id = prompt('Idingizni kiriting')
+let password = prompt('Passwordni kiriting')
+
+const obj = {
+    id: id,
+    names: fert,
+    password: password,
+  
+}
+
+console.log(obj);
